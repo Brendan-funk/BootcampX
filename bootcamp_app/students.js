85fd8a437faf0ebe910e326d0dd46ebf73d6950c
Brendan-funk <brendanzfunk@gmail.com>
2022-03-09 23:24:43 +0000

    adds security to the js queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const params = process.argv.slice(2);
-const cohortName = params[0];
-const max = params[1];
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${max || 5};
-`)
+const cohortName = params[0] || 'FEB';
+const max = params[1] || 5;
+const values = [`%${cohortName}%`, max];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+})
+.catch(err => {console.error('query-error', err.stack)});
